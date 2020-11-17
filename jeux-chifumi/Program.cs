425a7644cec05c808c4c42d08a8ed5e1a3c1e92a
osalysdecatarina <chemshoflack8@gmail.com>
2020-11-17 15:26:54 +0100

    Update Program.cs

--- jeux-chifumi/Program.cs
+++ jeux-chifumi/Program.cs
@@ -6,9 +6,25 @@ namespace jeux_chifumi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Choisissez votre un nombre");
+            int nbot;
+            int nut;
+            int res;
+            Console.WriteLine("Jeu du 0-2 \nBot, choisis un nombre entre 0 et 2"); //Intro 
+            Random nrand = new Random(); // Déclaration d'une variable aléatoire 
+            nbot = nrand.Next(0, 3); // L'ordinateur choisit une variable aléatoire
+            Console.WriteLine("Le nombre choisi par le bot est: " + nbot + "\nChoisissez à présent un nombre entre 0 et 2"); // Vérification et demande à l'utilisateur
+            nut = int.Parse(Console.ReadLine()); // Définition de la variable "nut" comme le chiffre choisi par l'utilisateur 
+            if (nbot < nut)
+            {
+                res = nut - nbot;
+            }
+            else if (nut < nbot) 
+            {
+                res = nbot - nut;
+            }
 
+           
+            
         }
     }
 }

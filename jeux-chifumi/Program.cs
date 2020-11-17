7e4050cb0f4485c2e81ba8134af0aa22e885c549
osalysdecatarina <chemshoflack8@gmail.com>
2020-11-17 09:49:56 +0100

    ajout commande utilisateur

--- jeux-chifumi/Program.cs
+++ jeux-chifumi/Program.cs
@@ -7,6 +7,8 @@ namespace jeux_chifumi
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Choisissez votre un nombre");
+
         }
     }
 }
